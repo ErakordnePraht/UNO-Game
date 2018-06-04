a3465328a78cf5255801b7854b35df9d1d7e7e45
Truberton <gert-andry.kaaramees@tptlive.ee>
2018-06-04 12:30:42 +0300

    Card deck in progress

---
+++ UNO-Game/Assets/Scripts/Card deck.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Card[] Deck = new Card[] { };
+	}
+}
+
+public class Card
+{
+    public string Number { get; set; }
+    public string Color { get; set;}
+}
+
+public class SpecialCard : Card
+{
+    public string SpecialFunction { get; set; }
+}
