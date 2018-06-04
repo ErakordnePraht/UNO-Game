ddf30cde8063eecf98317be6689b82048775aba8
ForTakEr <simotonu.prees@tptlive.ee>
2018-06-04 21:02:14 +0300

    You can no longer take the card from the table and put it in your hand

--- UNO-Game/Assets/Scripts/Draggable.cs
+++ UNO-Game/Assets/Scripts/Draggable.cs
@@ -3,12 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class Draggable : MonoBehaviour, IDragHandler
+public class Draggable : MonoBehaviour,IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    public Transform parentToReturnTo = null;
+
+    public enum Slot { TableCard, HandCard};
+    public Slot typeOfItem = Slot.TableCard;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        parentToReturnTo = transform.parent;
+        transform.SetParent(transform.parent.parent);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
 
     public void OnDrag(PointerEventData eventData)
     {
         transform.position = eventData.position;
     }
 
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.SetParent(parentToReturnTo);
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
 }
---
+++ UNO-Game/Assets/Scripts/DropZone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler
+{
+    public Draggable.Slot typeOfItem = Draggable.Slot.TableCard;
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log(eventData.pointerDrag.name + " Was dropped on " + gameObject.name);
+
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null)
+        {
+            if (typeOfItem == d.typeOfItem)
+            {
+                d.parentToReturnTo = transform;
+            }
+        }
+    }
+}
