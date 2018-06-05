ffba0d86700ac8245a41102ad02e78748295d5ca
Truberton <gertandry@gmail.com>
2018-06-05 17:29:56 +0300

    Fast commit incase i lose something

--- UNO-Game/Assets/Scripts/DrawCard.cs
+++ UNO-Game/Assets/Scripts/DrawCard.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class DrawCard : MonoBehaviour {
+public class DrawCard : MonoBehaviour, IBeginDragHandler, IDragHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("REEEEEEE");
+    }
 
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("AAAAAAAAAAAH");
+    }
     private void OnMouseDown()
     {
-        Debug.Log("REEEEEEEEEEE");
+        Debug.Log("AFHGUIAEGiuyguygs");
     }
 }
