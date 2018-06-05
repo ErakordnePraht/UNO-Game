d6eff6914dfc36e7913a1ced95b65acb48b6c668
ForTakEr <simotonu.prees@tptlive.ee>
2018-06-05 11:06:23 +0300

    Added animation to card drag

--- UNO-Game/Assets/Scripts/Draggable.cs
+++ UNO-Game/Assets/Scripts/Draggable.cs
@@ -2,17 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Draggable : MonoBehaviour,IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public Transform parentToReturnTo = null;
+    public Transform placeholderParent = null;
+
+    GameObject placeholder = null;
 
     public enum Slot { TableCard, HandCard};
     public Slot typeOfItem = Slot.TableCard;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
+        placeholder = new GameObject();
+        placeholder.transform.SetParent(transform.parent);
+        LayoutElement layoutElement = placeholder.AddComponent<LayoutElement>();
+        layoutElement.preferredWidth = GetComponent<LayoutElement>().preferredWidth;
+        layoutElement.preferredHeight = GetComponent<LayoutElement>().preferredHeight;
+        layoutElement.flexibleWidth = 0;
+        layoutElement.flexibleHeight = 0;
+
+        placeholder.transform.SetSiblingIndex(transform.GetSiblingIndex());
+
         parentToReturnTo = transform.parent;
+        placeholderParent = parentToReturnTo;
         transform.SetParent(transform.parent.parent);
 
         GetComponent<CanvasGroup>().blocksRaycasts = false;
@@ -21,11 +36,37 @@ public class Draggable : MonoBehaviour,IBeginDragHandler, IDragHandler, IEndDrag
     public void OnDrag(PointerEventData eventData)
     {
         transform.position = eventData.position;
+
+        if (placeholder.transform.parent != placeholderParent)
+        {
+            placeholder.transform.SetParent(placeholderParent);
+        }
+
+        int NewSiblingIndex = placeholderParent.childCount;
+
+        for (int i = 0; i < placeholderParent.childCount; i++)
+        {
+            if (transform.position.x < placeholderParent.GetChild(i).position.x)
+            {
+                NewSiblingIndex = i;
+
+                if (placeholder.transform.GetSiblingIndex() < NewSiblingIndex)
+                {
+                    NewSiblingIndex--;
+                }
+
+                break;
+            }
+        }
+        placeholder.transform.SetSiblingIndex(NewSiblingIndex);
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         transform.SetParent(parentToReturnTo);
+        transform.SetSiblingIndex(placeholder.transform.GetSiblingIndex());
         GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+        Destroy(placeholder);
     }
 }
--- UNO-Game/Assets/Scripts/DropZone.cs
+++ UNO-Game/Assets/Scripts/DropZone.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DropZone : MonoBehaviour, IDropHandler
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
 {
     public Draggable.Slot typeOfItem = Draggable.Slot.TableCard;
     public void OnDrop(PointerEventData eventData)
     {
-        Debug.Log(eventData.pointerDrag.name + " Was dropped on " + gameObject.name);
+        //Debug.Log(eventData.pointerDrag.name + " Was dropped on " + gameObject.name);
 
         Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
         if (d != null)
@@ -17,6 +17,34 @@ public class DropZone : MonoBehaviour, IDropHandler
             {
                 d.parentToReturnTo = transform;
             }
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null)
+        {
+            return;
+        }
+
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null)
+        {
+            d.placeholderParent = transform;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (eventData.pointerDrag == null)
+        {
+            return;
+        }
+
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null && d.placeholderParent == transform)
+        {
+            d.placeholderParent = d.parentToReturnTo;
         }
     }
 }
