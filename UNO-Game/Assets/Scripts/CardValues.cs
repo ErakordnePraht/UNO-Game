b9291206a7ff60d0c7403b5dc25d211a7de5c936
Truberton <gert-andry.kaaramees@tptlive.ee>
2018-06-05 11:29:56 +0300

    Added CardValues class

---
+++ UNO-Game/Assets/Scripts/CardValues.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardValues : MonoBehaviour {
+
+    public string Color;
+    public string Number;
+    public string SpecialFunction;
+}
