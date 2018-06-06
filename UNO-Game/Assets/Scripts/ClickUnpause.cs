ac92400cdd9092c0e0b8ca78a3293545466061f5
Truberton <gert-andry.kaaramees@tptlive.ee>
2018-06-06 10:55:16 +0300

    Pause menu added
    TODO - exit to title screen

---
+++ UNO-Game/Assets/Scripts/ClickUnpause.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickUnpause : MonoBehaviour
+{
+    public GameObject canvas;
+    public Button button;
+    void Update()
+    {
+        button.onClick.AddListener(OnClick);
+    }
+
+    void OnClick()
+    {
+        canvas.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
---
+++ UNO-Game/Assets/Scripts/ExitToDesktop.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitToDesktop : MonoBehaviour
+{
+    public Button button;
+    void Update()
+    {
+        button.onClick.AddListener(OnClick);
+    }
+    void OnClick()
+    {
+        Application.Quit();
+    }
+
+}
