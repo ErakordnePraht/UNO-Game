1fcbbd8811dc1cd5504eb887f78e0c16cf91d13a
Box-Bop <33263090+Box-Bop@users.noreply.github.com>
2018-06-06 08:26:35 +0300

    Doc

--- UNO-Game/Assets/Scripts/Card_deck.cs
+++ UNO-Game/Assets/Scripts/Card_deck.cs
@@ -142,6 +142,11 @@ public class SpecialCard : Card
 
 static class MyExtensions
 {
+    /// <summary>
+    /// Shuffles a list.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="list"></param>
     public static void Shuffle<T>(this IList<T> list)
     {
         int n = list.Count;
