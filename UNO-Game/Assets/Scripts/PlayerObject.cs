503aa4d724dac84cf37d9b892bbabddbf2bb5d84
ForTakEr <simotonu.prees@tptlive.ee>
2018-06-06 16:23:20 +0300

    IN PROGRESS

---
+++ UNO-Game/Assets/Scripts/PlayerObject.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerObject : NetworkBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (!isLocalPlayer)
+        {
+            //PlayerObject belongs to another player.
+            return;
+        }
+
+        CmdSpawnMyUnit();
+	}
+
+    public GameObject PlayerPrefab;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+	}
+
+    GameObject myPlayerUnit;
+
+    [Command]
+    public void CmdSpawnMyUnit()
+    {
+        GameObject go = Instantiate(PlayerPrefab);
+
+        myPlayerUnit = go;
+
+        NetworkServer.Spawn(go);
+    }
+}
