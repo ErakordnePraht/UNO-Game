0469f2df999101c3f579d739fa9404888338acde
Box-Bop <33263090+Box-Bop@users.noreply.github.com>
2018-06-07 08:53:20 +0300

    a bit of documentation

--- UNO-Game/Assets/Scripts/ExitToDesktop.cs
+++ UNO-Game/Assets/Scripts/ExitToDesktop.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// Closes the app, after the user has clicked on the "Exit to desktop" button.
+/// </summary>
 public class ExitToDesktop : MonoBehaviour
 {
     public Button button;
